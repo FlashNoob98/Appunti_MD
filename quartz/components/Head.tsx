d0a48404d13a14a0a268d44ce298fe62f15905d4
Daniele Olivieri <danieledaniele1988@gmail.com>
2024-11-06 11:18:54 +0100

    we have ads?

--- quartz/components/Head.tsx
+++ quartz/components/Head.tsx
@@ -46,6 +46,8 @@ export default (() => {
         {js
           .filter((resource) => resource.loadTime === "beforeDOMReady")
           .map((res) => JSResourceToScriptElement(res, true))}
+          <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1853420323386888"
+     crossorigin="anonymous"></script>
       </head>
     )
   }
