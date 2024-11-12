2eb54b4da95a492e6105a3cdcf7c261bd9eccba8
Daniele Olivieri <danieledaniele1988@gmail.com>
2024-11-12 17:38:41 +0100

    altre ads

--- quartz/components/pages/Content.tsx
+++ quartz/components/pages/Content.tsx
@@ -5,7 +5,10 @@ const Content: QuartzComponent = ({ fileData, tree }: QuartzComponentProps) => {
   const content = htmlToJsx(fileData.filePath!, tree)
   const classes: string[] = fileData.frontmatter?.cssclasses ?? []
   const classString = ["popover-hint", ...classes].join(" ")
-  return <article class={classString}>{content}</article>
+  return <article class={classString}>
+  <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1853420323386888"
+     crossorigin="anonymous"></script>
+     {content}</article>
 }
 
 export default (() => Content) satisfies QuartzComponentConstructor
